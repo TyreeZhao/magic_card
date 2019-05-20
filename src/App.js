c2ac08643438362282dbdefdd751b1ebd81613a1
ZhaoTong <zhaotong@meican.com>
2019-05-20 16:53:34 +0800

    Feat: magic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react"
+// import logo from "./logo.svg"
+import "./App.css"
 
-function App() {
+const App = props => {
+  const poster = React.createRef()
+  const { children } = props
+  useEffect(() => {
+    window.addEventListener("mousemove", mousemove)
+    return () => {
+      window.removeEventListener("mousemove", mousemove)
+    }
+  })
+
+  const mousemove = e => {
+    const {
+      left,
+      top,
+      width,
+      height,
+      right,
+      bottom
+    } = poster.current.getBoundingClientRect()
+    var innerLeft = e.pageX - left < 0 ? 0 : e.pageX - left
+    var innerTop = e.pageY - top < 0 ? 0 : e.pageY - top
+    var offsetX = 0
+    var offsetY = 0
+
+    if (innerLeft > 0 && innerTop > 0 && e.pageX < right && e.pageY < bottom) {
+      offsetX = 0.5 - innerLeft / width
+      offsetY = 0.5 - innerTop / height
+    }
+
+    var offsetPoster = -10
+    var transformPoster =
+      "translateY(" +
+      -offsetX * offsetPoster +
+      "px) rotateX(" +
+      -offsetY * offsetPoster +
+      "deg) rotateY(" +
+      offsetX * offsetPoster +
+      "deg)"
+    poster.current.style.transform = transformPoster
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={poster} className="poster">
+      {children}
     </div>
-  );
+  )
 }
-
-export default App;
+export default App

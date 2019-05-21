11c936c3996fb648bd4193af27245ca92a881873
ZhaoTong <zhaotong@meican.com>
2019-05-21 17:08:11 +0800

    Fix: tunning offset

--- src/App.js
+++ src/App.js
@@ -1,63 +1,57 @@
-import React, { useEffect } from "react";
-import "./App.css";
+import React, { useEffect } from "react"
+import "./App.css"
 
 const MagicCard = props => {
-  const poster = React.createRef();
-  const innerPoster = React.createRef();
+  const poster = React.createRef()
+  const innerPoster = React.createRef()
 
-  const { children } = props;
+  const { children } = props
 
   useEffect(() => {
-    window.addEventListener("mousemove", mousemove);
+    window.addEventListener("mousemove", mousemove)
     return () => {
-      window.removeEventListener("mousemove", mousemove);
-    };
-  });
+      window.removeEventListener("mousemove", mousemove)
+    }
+  })
 
   const mousemove = e => {
-    const { left, top, width, height } = poster.current.getBoundingClientRect();
-    var innerLeft = e.pageX - left < 0 ? 0 : e.pageX - left;
-    var innerTop = e.pageY - top < 0 ? 0 : e.pageY - top;
-    var offsetX = 0;
-    var offsetY = 0;
-    var transformPoster = "";
-    var childrenTransform = "";
-    var offsetPoster = -10;
-    var offsetLayer = -17;
+    const {
+      left,
+      top,
+      width,
+      height,
+      right,
+      bottom
+    } = poster.current.getBoundingClientRect()
+    var innerLeft = e.pageX - left < 0 ? 0 : e.pageX - left
+    var innerTop = e.pageY - top < 0 ? 0 : e.pageY - top
+    var offsetX = 0
+    var offsetY = 0
 
-    // if (innerLeft > 0 && innerTop > 0 && e.pageX < right && e.pageY < bottom) {
-    offsetX = 0.5 - innerLeft / width;
-    offsetY = 0.5 - innerTop / height;
+    if (innerLeft > 0 && innerTop > 0 && e.pageX < right && e.pageY < bottom) {
+      offsetX = 0.5 - innerLeft / width
+      offsetY = 0.5 - innerTop / height
+    }
 
-    transformPoster =
-      "scale(1.05, 1.05)" +
+    var offsetPoster = -8
+    var transformPoster =
       "translateY(" +
-      (-offsetX * offsetPoster) / 2 +
-      "px) rotateX(" + // 逆时针
-      offsetY * offsetPoster + // 顺时针
+      offsetY * offsetPoster +
+      "px) translateX(" +
+      offsetX * offsetPoster +
+      "px) rotateX(" +
+      (offsetY * offsetPoster) / 2 +
       "deg) rotateY(" +
-      offsetX * -offsetPoster * 2 +
-      "deg)";
-
-    childrenTransform =
-      "translateX(" +
-      -offsetX * offsetLayer +
-      "px) translateY(" +
-      -offsetY * offsetLayer +
-      "px)";
-    // } else {
-    //   transformPoster = "";
-    //   childrenTransform = "";
-    // }
-    poster.current.style.transform = transformPoster;
-    innerPoster.current.style.transform = childrenTransform;
-  };
+      -offsetX * offsetPoster +
+      "deg)"
+    poster.current.style.transform = transformPoster
+  }
   return (
     <div ref={poster} className={`poster ${props.className}`}>
       <div ref={innerPoster}>{children}</div>
     </div>
-  );
-};
+  )
+}
 
 const Home = props => {
   return (
@@ -66,6 +60,6 @@ const Home = props => {
         <div>123</div>
       </MagicCard>
     </div>
-  );
-};
-export default Home;
+  )
+}
+export default Home
